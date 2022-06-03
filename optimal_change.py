10833482033c569cb2aadbf489701192180941e3
Garrett Adams <garrett.adams52@gmail.com>
2022-06-03 16:52:03 -0400

    Finished Assessment

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,30 @@
-# Write your solution here!
+def optimal_change(item_cost, amount_paid):
+
+    #check that change is required
+    if item_cost >= amount_paid : return "You are owed no change" 
+    
+    #variables
+    values = {100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 1: 0,.25: 0, .10: 0, .05: 0, .01: 0} #values and their count
+    units = {.25: 'quarter', .10: 'dime', .05: 'nickel', .01: 'penny'} #titles of units
+    ans = f'The optimal change for an item that costs ${item_cost:.2f} with an amount paid of ${amount_paid:.2f} is' #beginning of return string
+    change = amount_paid - item_cost # calc required change
+
+    #calc number of bills/coins and store in object
+    while change >= .01:
+        for k in values:
+            if change >= k:
+                values[k] = values[k] + 1
+                change = round(change - k, 2)
+                break
+
+    #reduce object to exclude bills/coins not used
+    values = {k: v for k, v in values.items() if v != 0}
+
+    #print info from reduced value dict
+    for i, k in enumerate(values):
+        unit = 'bill' if k >=1 else units[k]
+        if unit == 'penny' and values[k] > 1:
+            unit = 'pennies'
+        ans = ans + f" {'and ' if (len(values) > 1 and i == len(values)-1) else ''}{values[k]} {f'${k} ' if k >= 1 else ''}{unit}{'s' if (values[k] != 1 and k!=0.01) else ''}{'.' if (i == len(values)-1) else ','}"
+    
+    return ans
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,16 @@
 # Write your tests here!
+from optimal_change import optimal_change
+import random
+import math
+
+print(optimal_change(31.49, 50) == 'The optimal change for an item that costs $31.49 with an amount paid of $50.00 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 2 quarters, and 1 penny.')
+print(optimal_change(.01, .01) == 'You are owed no change')
+print(optimal_change(189.01, 256.89) == 'The optimal change for an item that costs $189.01 with an amount paid of $256.89 is 1 $50 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 3 pennies.')
+print(optimal_change(6, 6.75) == 'The optimal change for an item that costs $6.00 with an amount paid of $6.75 is 3 quarters.')
+print(optimal_change(.05, .01) == 'You are owed no change')
+
+# count = 0.01
+# while count < 100:
+#     rand = random.randrange(0,math.ceil(count), 1)
+#     print(optimal_change(rand, round(count, 2)))
+#     count+=0.01
